2313d29ed2fb1ee10435798d903abc14bf149d5a
Your Name <you@example.com>
2022-02-14 22:11:33 +0100

    add player

--- script.js
+++ script.js
@@ -4,14 +4,19 @@ let current_shape = 'cross';
 function fill_shape(id) {
     if (current_shape == 'cross') {
         current_shape = 'circle';
+        document.getElementById('player_2').classList.remove('player-inactive');
+        document.getElementById('player_1').classList.add('player-inactive');
     } 
     else {
         current_shape = 'cross';
+        document.getElementById('player_1').classList.remove('player-inactive');
+        document.getElementById('player_2').classList.add('player-inactive');
     }
 
     fields[id] = current_shape;
     console.log(fields);
     draw();
+    check_winner();
 }
 
 function draw() {
@@ -25,4 +30,37 @@ function draw() {
         }
     }
 
+}
+
+function check_winner() {
+    let winner; 
+
+    if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
+        winner = fields[0];
+    }
+    else if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
+        winner = fields[3];
+    }
+    else if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
+        winner = fields[6];
+    }
+    else if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
+        winner = fields[0];
+    }
+    else if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
+        winner = fields[1];
+    }
+    else if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
+        winner = fields[2];
+    }
+    else if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
+        winner = fields[0];
+    }
+    else if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
+        winner = fields[2];
+    }
+
+    if(!!winner) {
+        console.log('GEWONNEN:', winner);
+    }
 }
